21a5d72db5cc8e6cbc0d4276320385449028a028
alexis <adelaforge@lirmm.fr>
2020-04-14 21:48:17 +0200

    Success false test 6

--- nodejs-project/suite_decimee.spec.js
+++ nodejs-project/suite_decimee.spec.js
@@ -20,7 +20,7 @@ try {
   success++;
 } catch (error) {
   printMessage('Standard Output💡', 'Encore un petit effort.  🤔');
-  //throw "Tous les tests ne sont pas justes.!";
+  console.error("Tous les tests ne sont pas justes!");
 }
 
 
